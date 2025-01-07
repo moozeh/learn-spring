eff85a335c2188f040e29caa13f3aaddc6a3a9b7
Chanwoo Kim <kcw2205@naver.com>
2025-01-07 23:34:16 +0900

    project2: create spring bean example

--- src/main/java/org/moozeh/learn_spring_framework/App01GamingBasicJava.java
+++ src/main/java/org/moozeh/learn_spring_framework/App01GamingBasicJava.java
@@ -1,10 +1,9 @@
 package org.moozeh.learn_spring_framework;
 
 import org.moozeh.learn_spring_framework.game.GameRunner;
-import org.moozeh.learn_spring_framework.game.MarioGame;
 import org.moozeh.learn_spring_framework.game.PacmanGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
     public static void main(String[] args) {
 
         // var game = new MarioGame();
